a5bc8643c12af26c6993f44e2944c0e49d16d9bb
codedim <codedim@gmail.com>
2016-11-28 13:25:25 +0300

    the first version

---
+++ README.md
@@ -0,0 +1,68 @@
+## What is GTA?
+
+GTA is a Google Translate Assistant extension for the Google Chrome browser. 
+The extension was specially designed to help with learning a foreign language 
+using the great Google Translate service.
+
+![](./Info/gta.01.png =480x)
+
+Let's look at the service more carefully. What's wrong with it? It's pretty 
+nothing if you are using a mouse. So, what about if you want to use only 
+keyboard to work with it? From this point of view it doesn't look very friendly.
+But, why? Let's see.
+
+![](./Info/gta.02.png =480x)
+
+When you start typing in any text Google immediately brings a list of suggestions 
+to you. It's very useful. But, there's a problem, you can't see phonetic notation 
+(transcription) anymore and you aren't able to access the buttons - for example, 
+to play pronunciation sounds - before you've pressed __Esc__ key to remove the 
+suggestion list.
+
+![](./Info/gta.03.png =480x)
+
+An another essential problem is that you can't switch languages without your 
+mouse. But, what about if we might be able to see the transcription all the time 
+and also have some useful keyboard shortcuts to switch languages, play 
+pronunciation, etc? Sounds good? Well, let's go ahead.
+
+![](./Info/gta.04.png =480x)
+
+After you has installed the GTA extension, you will be able to see the 
+transcription instead of the '__Translate__' header and get the following 
+shortcuts:
+
+* **Ctrl + 'M'** - to activate/disactivate microphone;
+* **Ctrl + ' , '** - to play the left side source text;
+* **Ctrl + ' . '** - to play the right side result text;
+* **Ctrl + ' / '** and **Shift + Alt** - to switch between languages.
+
+> **Note:** The **Shift + Alt** shortcut is more useful when you need 
+to switch between your favorite languages in the Google service, because in this 
+way you can also switch between keyboard input languages and start typing with the 
+different language immediately. Use the **Ctrl + ' / '** shortcut if you just 
+need to switch languages in the service only.
+
+## How to install?
+
+You can have this extension installed only in **Developer mode** currently. 
+To do that click the "__Customize and control Google Chrome__" button and select 
+"__More tools -> Extensions__", then enable the "__Developer mode__" and 
+press the "__Load unpacked extension__" button. Point the folder when you have 
+downloaded this repository.
+
+![](./Info/gta.05.png =480x)
+
+Finally, reload the Google Translate service page.
+
+## FAQ
+
+**Why isn't any customization of shortcuts provided?**
+
+The code is pretty simple, so you can change it as you need. Maybe in the next 
+release.
+
+**Why is the developer mode needed to be used?**
+
+I haven't yet plans to post the utility on the Google store. So, apologizing for 
+this inconvenience.
---
+++ gtassistant.js
@@ -0,0 +1,140 @@
+/*
+**  
+**  
+**  
+*/
+
+// key code constants
+const kcShift =  16;
+const kcCtrl  =  17;
+const kcAlt   =  18;
+const kcMKey  =  77;
+const kcComma = 188;
+const kcDot   = 190;
+const kcSlash = 191;
+
+// global variables, control and information elements
+var translitElem;    // phonetic notation
+var appnameElem;     // 'translator' header
+var swapElem;        // language switcher
+var speechElem;      // microphone button
+var srcListenElem;   // source speaker button
+var resListenElem;   // result speaker button
+var srcTextArea;     // input source textarea
+// keyboard keydown and keyup control
+var keyArray = [];   // array of pressed keys 
+
+
+window.onload = function() {
+	// find all information elements
+	translitElem = document.getElementById('src-translit');
+	appnameElem  = document.getElementById('gt-appname');
+	// find all control elements
+	swapElem      = document.getElementById('gt-swap');
+	speechElem    = document.getElementById('gt-speech');
+	srcListenElem = document.getElementById('gt-src-listen');
+	resListenElem = document.getElementById('gt-res-listen');
+	srcTextArea   = document.getElementById('source');
+	
+	if (translitElem && appnameElem && swapElem &&
+		speechElem && srcListenElem && resListenElem) 
+	{
+		window.setInterval(dispatchTranslate, 100);	
+	} else {
+		if (!translitElem) 
+			console.log('Error: Phonetic notation element was not found!');
+		if (!appnameElem) 
+			console.log('Error: "Translator" header element was not found!');
+		if (!swapElem) 
+			console.log('Error: Language switcher element was not found!');
+		if (!speechElem) 
+			console.log('Error: Microphone button element was not found!');
+		if (!srcListenElem) 
+			console.log('Error: Source speaker button element was not found!');
+		if (!resListenElem) 
+			console.log('Error: Result speaker button element was not found!');
+		if (!srcTextArea) 
+			console.log('Error: Source textarea element was not found!');
+	}
+
+	setupMouseEventEmulator();
+}
+
+window.onfocus = function() {
+	keyArray = [];
+	// set focus to source input textarea
+	srcTextArea.focus();
+}
+
+window.onblur = function() {
+	keyArray = [];
+}
+
+window.addEventListener("keydown", function(event) {
+	processKeyEvent(event.keyCode, true);
+});
+
+window.addEventListener("keyup", function(event) {
+	processKeyEvent(event.keyCode, false);
+});
+
+
+var mouseEvent = [];
+
+function setupMouseEventEmulator() {
+	var events = ['mousedown', 'mouseup', 'mouseout', 'mouseover', 'click'];
+
+	for (var i = 0, e; e = events[i++];) {
+		mouseEvent[e] =  document.createEvent('MouseEvents');
+		mouseEvent[e].initEvent(e, true, false);
+	}
+}
+
+
+function processKeyEvent(keyCode, isKeyDown) {
+	// process shortcuts
+	var controlButton;
+	if (!isKeyDown && keyArray.length == 2) {
+	 	if (keyArray[0] == kcCtrl && keyArray[1] == kcMKey)
+	 		controlButton = speechElem;
+	 	if (keyArray[0] == kcCtrl && keyArray[1] == kcComma)  
+	 		controlButton = srcListenElem;
+	 	if (keyArray[0] == kcCtrl && keyArray[1] == kcDot)    
+	 		controlButton = resListenElem;
+	 	if (keyArray[0] == kcCtrl && keyArray[1] == kcSlash)  
+	 		controlButton = swapElem;
+	 	// on switch input languages
+	 	if (keyArray[0] == kcShift && keyArray[1] == kcAlt || 
+	 		keyArray[0] == kcAlt && keyArray[1] == kcShift)
+	 		controlButton = swapElem;
+	} 
+
+	// do the job
+	if (controlButton) {
+		var dispatchEvent = controlButton.dispatchEvent.bind(controlButton);
+		dispatchEvent(mouseEvent['mouseover']);
+		dispatchEvent(mouseEvent['mousedown']);
+		dispatchEvent(mouseEvent['mouseup']);
+		dispatchEvent(mouseEvent['mouseout']);
+	}
+
+	// update key array
+	if (isKeyDown) {
+		// prevent multy-keydawn events
+		if (keyArray[keyArray.length - 1] !== keyCode) 
+			keyArray.push(keyCode);
+	} else {
+		keyArray.pop();
+	}
+}
+
+
+function dispatchTranslate() {
+	if (appnameElem.innerHTML !== translitElem.innerHTML) {
+		appnameElem.innerHTML = translitElem.innerHTML;
+	}
+
+	// for debug only, must be commented!
+/*	translitElem.innerHTML = String(keyArray);  */
+}
+
